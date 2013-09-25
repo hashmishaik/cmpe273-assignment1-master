691cea223277e1cc0f8e76d9c9d9b89aecb1d7e8
HashmiShaik <itsmehashu@gmail.com>
2013-09-24 23:10:16 -0700

    Final commit!

---
+++ src/main/java/edu/sjsu/cmpe/library/repository/BookRepository.java
@@ -0,0 +1,93 @@
+package edu.sjsu.cmpe.library.repository;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.sjsu.cmpe.library.domain.Author;
+import edu.sjsu.cmpe.library.domain.Book;
+import edu.sjsu.cmpe.library.dto.AuthorDto;
+import edu.sjsu.cmpe.library.dto.BooksDto;
+import edu.sjsu.cmpe.library.dto.LinkDto;
+
+public class BookRepository implements BookRepositoryInterface {
+    /** In-memory map to store books. (Key, Value) -> (ISBN, Book) */
+    private final ConcurrentHashMap<Long, Book> bookInMemoryMap;
+
+    /** Never access this key directly; instead use generateISBNKey() */
+    private long isbnKey;
+
+    public BookRepository(ConcurrentHashMap<Long, Book> bookMap) {
+	checkNotNull(bookMap, "bookMap must not be null for BookRepository");
+	bookInMemoryMap = bookMap;
+	isbnKey = 0;
+    }
+
+    /**
+     * This should be called if and only if you are adding new books to the
+     * repository.
+     * 
+     * @return a new incremental ISBN number
+     */
+    private final Long generateISBNKey() {
+	
+	return Long.valueOf(++isbnKey);
+    }
+
+    /**
+     * This will auto-generate unique ISBN for new books.
+     */
+    @Override
+    public Book saveBook(Book newBook) {
+	checkNotNull(newBook, "newBook instance must not be null");
+	
+	Long isbn = generateISBNKey();
+	newBook.setIsbn(isbn);
+	
+	ArrayList<Author> authors = newBook.getAuthors();
+	int i = 0;
+	
+	for (Author item: authors) {
+		item.setId(++i);
+	}
+
+
+	
+	bookInMemoryMap.putIfAbsent(isbn, newBook);
+
+	return newBook;
+    }
+
+    /**
+     * @see edu.sjsu.cmpe.library.repository.BookRepositoryInterface#getBookByISBN(java.lang.Long)
+     */
+    @Override
+    public Book getBookByISBN(Long isbn) {
+	checkArgument(isbn > 0,
+		"ISBN was %s but expected greater than zero value", isbn);
+	     return bookInMemoryMap.get(isbn);
+	     
+		
+    }
+    
+    @Override
+    public Book removeBookByISBN(Long isbn) {
+    checkArgument(isbn > 0, "ISBN was %s but expected greater than zero value", isbn);
+    Book previousValue = bookInMemoryMap.remove(isbn);
+    	return previousValue;
+    }
+    
+    public void updateBookInfo(Book book, Entry<String, List<String>> entry) {
+    	String str= entry.getValue().toString();
+    	if (entry.getKey().equals("status")) {
+    		book.setStatus(str.substring(1,str.length()-1));
+    	}
+    	else if (entry.getKey().equals("title")) {
+    		book.setStatus(str.substring(1,str.length()-1));
+    	}
+    }
+}
---
+++ src/main/java/edu/sjsu/cmpe/library/repository/ReviewRepository.java
@@ -0,0 +1,83 @@
+package edu.sjsu.cmpe.library.repository;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.sjsu.cmpe.library.domain.Review;
+
+public class ReviewRepository implements ReviewRepositoryInterface {
+	/** In-memory map to store books. (Key, Value) -> (Id, Review) */
+	private final ConcurrentHashMap<Integer, Review> reviewInMemoryMap;
+
+
+	/** Never access this key directly; instead use generateISBNKey() */
+	private Integer id;
+
+
+	public ReviewRepository(ConcurrentHashMap<Integer,Review> reviewMap) {
+		checkNotNull(reviewMap, "reviewMap must not be null for BookRepository");
+		reviewInMemoryMap = reviewMap;
+		id = 0;
+	}
+
+	/**
+	 * This should be called if and only if you are adding a new review
+	 * 
+	 * 
+	 * @return a new incremental Id
+	 */
+	private final Integer generateId() {
+		
+		return Integer.valueOf(++id);
+	}
+
+	/**
+	 * This will auto-generate unique review ID
+	 */
+	@Override
+	public Review saveReview(Review newReview) {
+		checkNotNull(newReview, "newReview instance must not be null");
+		
+		Integer id = generateId();
+		newReview.setId(id);
+		
+		reviewInMemoryMap.putIfAbsent(id,newReview);
+
+		return newReview;
+	}
+
+	@Override
+	public Review getReviewById(Integer id) {
+		checkArgument(id > 0,
+				"ISBN was %s but expected greater than zero value", id);
+		return reviewInMemoryMap.get(id);
+
+	}
+
+	@Override
+	public ArrayList<Review> getAllReviews() {
+
+		ArrayList<Review> arrayList = new ArrayList<Review>();
+
+		for( id=1; id<=reviewInMemoryMap.size() ; id++){
+
+			Review review = new Review();
+			review = reviewInMemoryMap.get(id);
+			arrayList.add(review);
+
+		}
+
+
+		return arrayList;
+
+
+
+
+	}
+
+
+
+}
